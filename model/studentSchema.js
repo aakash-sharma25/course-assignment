14f77a11d8ef4c88a2ef573203178078ff4937ea
aakash-sharma25 <as024harma@gmail.com>
2024-09-01 21:02:39 +0530

    auth route

--- model/studentSchema.js
+++ model/studentSchema.js
@@ -1,12 +1,15 @@
 // models/studentModel.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const StudentSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  enrolledCourses: [{
-    course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course' },
-    progress: { type: Number, default: 0 }
-  }],
+  email: { type: String, required: true },
+  enrolledCourses: [
+    {
+      course: { type: mongoose.Schema.Types.ObjectId, ref: "Course" },
+      progress: { type: Number, default: 0 },
+    },
+  ],
 });
 
-module.exports = mongoose.model('Student', StudentSchema);
+module.exports = mongoose.model("Student", StudentSchema);
--- router/studentRoutes.js
+++ router/studentRoutes.js
@@ -1,9 +1,10 @@
 // routes/studentRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getStudentDashboard, markCourseCompleted } = require('../controllers/studentControllers');
+const { getStudentDashboard, markCourseCompleted, buyCourse } = require('../controllers/studentControllers');
 
 router.get('/:id/dashboard', getStudentDashboard);
+router.post('/buy', buyCourse);
 router.post('/:studentId/courses/:courseId/complete', markCourseCompleted);
 
 module.exports = router;
