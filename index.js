b8b1431f00617b8ebaf50dd206de1999aefacde3
aakash-sharma25 <as024harma@gmail.com>
2024-09-01 19:42:50 +0530

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const courseRoutes = require("./router/courseRoutes");
 const studentRoutes = require("./router/studentRoutes");
+const path = require("path")
 
 const mongoose = require("mongoose");
 require("dotenv").config();
